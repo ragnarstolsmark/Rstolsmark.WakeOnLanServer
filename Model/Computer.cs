3d185c8ae661492c0fbd529082bf8bc99abbe948
Ragnar Stølsmark <rstolsmark@live.no>
2019-03-02 09:33:38 +0100

    Added ping functionality

--- Model/Computer.cs
+++ Model/Computer.cs
@@ -1,3 +1,6 @@
+using System.Net.NetworkInformation;
+using System.Threading.Tasks;
+
 namespace WakeOnLanServer.Model
 {
     public class Computer
@@ -5,6 +8,13 @@ namespace WakeOnLanServer.Model
         public string Name { get; set; }
         public string IP { get; set; }
         public string MAC { get; set; }
-        public bool Woken {get;set;}
+        public bool Woken {get; set;}
+
+        public async Task Ping(){
+            Ping pingSender = new Ping();
+            //Send with a low timeout since it is on LAN and to prevent slow page load if computers are offline
+            PingReply reply = await pingSender.SendPingAsync(IP, 50);
+            Woken = reply.Status == IPStatus.Success;
+        }
     }
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -11,18 +11,24 @@ namespace WakeOnLanServer.Pages
     public class IndexModel : PageModel
     {
         public static Computer[] COMPUTERS => new Computer[]{
-                new Computer {Name = "Ragnar", IP = "192.168.0.4", MAC = "FF:FF"},
-                new Computer {Name = "Joar", IP = "192.168.0.5", MAC = "FF:AE"}
+                new Computer {Name = "Ragnar", IP = "192.168.0.11", MAC = "FF:FF"},
+                new Computer {Name = "Joar", IP = "192.168.0.4", MAC = "FF:AE"}
             };
         public  Dictionary<string, Computer> Computers { get; set; }
-        public void OnGet()
+        public async Task OnGetAsync()
         {
-            Computers = COMPUTERS.ToDictionary(c=>c.Name);
+            Computers = COMPUTERS.ToDictionary(c => c.Name);
+            await PingAllComputers();
         }
 
-        public void OnPost(string computerToWake){
+        private Task PingAllComputers()
+        {
+            return Task.WhenAll(Computers.Values.Select(c => c.Ping()));
+        }
+
+        public async Task OnPostAsync(string computerToWake){
             Computers = COMPUTERS.ToDictionary(c=>c.Name);            
-            Computers[computerToWake].Woken = true;
+            await PingAllComputers();
         }
     }
 }
