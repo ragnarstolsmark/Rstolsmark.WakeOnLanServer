15082aea49b8305dbe2e0d7dad28152db96d43b2
Ragnar Stølsmark <ragnar@identitystream.com>
2019-03-04 09:40:29 +0100

    Updated mac addresses

--- Model/Computer.cs
+++ Model/Computer.cs
@@ -6,55 +6,41 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WakeOnLanServer.Model
-{
-    public class Computer
-    {
-        public string Name { get; set; }
-        public string IP { get; set; }
-        public string MAC { get; set; }
-        public bool Woken {get; set;}
+namespace WakeOnLanServer.Model {
+	public class Computer {
+		public string Name { get; set; }
+		public string IP { get; set; }
+		public string MAC { get; set; }
+		public bool Woken { get; set; }
 
-        public async Task Ping(){
-            Ping pingSender = new Ping();
-            //Send with a low timeout since it is on LAN and to prevent slow page load if computers are offline
-            PingReply reply = await pingSender.SendPingAsync(IP, 50);
-            Woken = reply.Status == IPStatus.Success;
-        }
+		public async Task Ping() {
+			Ping pingSender = new Ping();
+			//Send with a low timeout since it is on LAN and to prevent slow page load if computers are offline
+			PingReply reply = await pingSender.SendPingAsync(IP, 50);
+			Woken = reply.Status == IPStatus.Success;
+		}
 
-        public async Task WakeUp(){
-            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-            s.EnableBroadcast = true;
-            IPAddress broadcast = IPAddress.Parse("255.255.255.255");
-            byte[] prefix = new byte[]{ 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F };
-            var pureMac = MacToByteArrayToSend(MAC);
-            byte[] sendbuf = prefix;
-            for(int i=1;i<=16;i++){
-                sendbuf = sendbuf.Concat(pureMac).ToArray();
-            }
-            IPEndPoint ep = new IPEndPoint(broadcast, 9); 
-            await s.SendToAsync(sendbuf, SocketFlags.None,ep);
-            byte[] MacToByteArrayToSend(string mac)
-            {
-                byte[] bytes = new byte[6];
-                var hexStrings = mac.Split(":");                
-                for (int i = 0; i < 6; i++){
-                    bytes[i] = Convert.ToByte(hexStrings[i], 16);
-                }
-                if(BitConverter.IsLittleEndian){
-                    var tempByte = bytes[0];
-                    bytes[0] = bytes[1];
-                    bytes[1] = tempByte;
-                    tempByte = bytes[2];
-                    bytes[2] = bytes[3];
-                    bytes[3] = tempByte;
-                    tempByte = bytes[4];
-                    bytes[4] = bytes[5];
-                    bytes[5] = tempByte;
-                }
-                return bytes;
-            }
+		public async Task WakeUp() {
+			Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+			s.EnableBroadcast = true;
+			IPAddress broadcast = IPAddress.Parse("255.255.255.255");
+			byte[] prefix = new byte[] { 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F };
+			var pureMac = MacToByteArrayToSend(MAC);
+			byte[] sendbuf = prefix;
+			for (int i = 1; i <= 16; i++) {
+				sendbuf = sendbuf.Concat(pureMac).ToArray();
+			}
+			IPEndPoint ep = new IPEndPoint(broadcast, 9);
+			await s.SendToAsync(sendbuf, SocketFlags.None, ep);
+			byte[] MacToByteArrayToSend(string mac) {
+				byte[] bytes = new byte[6];
+				var hexStrings = mac.Split(":");
+				for (int i = 0; i < 6; i++) {
+					bytes[i] = Convert.ToByte(hexStrings[i], 16);
+				}
+				return bytes;
+			}
 
-        }
-    }
+		}
+	}
 }
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -6,31 +6,27 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using WakeOnLanServer.Model;
 
-namespace WakeOnLanServer.Pages
-{
-    public class IndexModel : PageModel
-    {
-        public static Computer[] COMPUTERS => new Computer[]{
-                new Computer {Name = "Ragnar", IP = "192.168.0.12", MAC = "F0:1F:AF:33:16:BB"},
-                new Computer {Name = "Joar", IP = "192.168.0.4", MAC = "3C:A9:F4:4E:32:8D"}
-            };
-        public  Dictionary<string, Computer> Computers { get; set; }
-        public async Task OnGetAsync()
-        {
-            Computers = COMPUTERS.ToDictionary(c => c.Name);
-            await PingAllComputers();
-        }
+namespace WakeOnLanServer.Pages {
+	public class IndexModel : PageModel {
+		public static Computer[] COMPUTERS => new Computer[]{
+				new Computer {Name = "Ragnar", IP = "192.168.5.66", MAC = "18:31:BF:25:39:4A"},
+				new Computer {Name = "Joar", IP = "192.168.5.156", MAC = "40:B0:76:0E:86:6B"}
+			};
+		public Dictionary<string, Computer> Computers { get; set; }
+		public async Task OnGetAsync() {
+			Computers = COMPUTERS.ToDictionary(c => c.Name);
+			await PingAllComputers();
+		}
 
-        private Task PingAllComputers()
-        {
-            return Task.WhenAll(Computers.Values.Select(c => c.Ping()));
-        }
+		private Task PingAllComputers() {
+			return Task.WhenAll(Computers.Values.Select(c => c.Ping()));
+		}
 
-        public async Task OnPostAsync(string computerToWake){
-            Computers = COMPUTERS.ToDictionary(c=>c.Name);            
-            //Use a discard since we don't need to await the wake up since it will not start up fast enough to reply to the next ping anyway
-            _ = Computers[computerToWake].WakeUp();            
-            await PingAllComputers();
-        }
-    }
+		public async Task OnPostAsync(string computerToWake) {
+			Computers = COMPUTERS.ToDictionary(c => c.Name);
+			//Use a discard since we don't need to await the wake up since it will not start up fast enough to reply to the next ping anyway
+			_ = Computers[computerToWake].WakeUp();
+			await PingAllComputers();
+		}
+	}
 }
