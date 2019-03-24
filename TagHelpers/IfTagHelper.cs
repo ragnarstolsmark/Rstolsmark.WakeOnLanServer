83b84a4cb02a9088de57e040ae0e5bdb2a5853e7
Ragnar Stølsmark <rstolsmark@live.no>
2019-03-24 08:38:34 +0100

    Added if tag helper

---
+++ TagHelpers/IfTagHelper.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WakeOnLanServer.TagHelpers
+{
+    [HtmlTargetElement(Attributes = nameof(If))]
+    public class IfTagHelper : TagHelper
+    {
+        public bool If { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!If)
+            {
+                output.SuppressOutput();
+            }
+        }
+    }
+}
