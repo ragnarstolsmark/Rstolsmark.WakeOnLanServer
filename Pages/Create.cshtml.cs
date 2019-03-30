5d5e5370b7da1ec38d5112616fb30443b2cdcbc8
Ragnar Stølsmark <rstolsmark@live.no>
2019-03-30 16:16:04 +0100

    Superbasic validation

--- Pages/Create.cshtml.cs
+++ Pages/Create.cshtml.cs
@@ -17,6 +17,10 @@ namespace WakeOnLanServer.Pages
         {
         }
         public IActionResult OnPost(){
+            if(string.IsNullOrWhiteSpace(Computer.Name) || string.IsNullOrWhiteSpace(Computer.IP) || string.IsNullOrWhiteSpace(Computer.MAC)){
+                TempData["Message"] = "Du må fylle ut alle feltene";
+                return Page();
+            }
             AddOrUpdateComputer(Computer);
             return RedirectToPage("/Index");
         }
