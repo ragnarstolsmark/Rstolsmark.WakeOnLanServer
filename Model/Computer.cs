ac9a587561af7662cf502b78bc69844b4730093c
Ragnar Stølsmark <rstolsmark@live.no>
2020-02-22 19:28:08 +0100

    Fixed broadcasting to correct subnet

--- Model/Computer.cs
+++ Model/Computer.cs
@@ -7,43 +7,52 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 
-namespace Rstolsmark.WakeOnLanServer.Model {
-	public class Computer {
-		public string Name { get; set; }
-		public string IP { get; set; }
-		public string MAC { get; set; }
+namespace Rstolsmark.WakeOnLanServer.Model
+{
+    public class Computer
+    {
+        public string Name { get; set; }
+        public string IP { get; set; }
+        public string MAC { get; set; }
 
-		[JsonIgnore]
-		public bool Woken { get; set; }
+        [JsonIgnore]
+        public bool Woken { get; set; }
 
-		public async Task Ping() {
-			Ping pingSender = new Ping();
-			//Send with a low timeout since it is on LAN and to prevent slow page load if computers are offline
-			PingReply reply = await pingSender.SendPingAsync(IP, 50);
-			Woken = reply.Status == IPStatus.Success;
-		}
+        public async Task Ping()
+        {
+            Ping pingSender = new Ping();
+            //Send with a low timeout since it is on LAN and to prevent slow page load if computers are offline
+            PingReply reply = await pingSender.SendPingAsync(IP, 50);
+            Woken = reply.Status == IPStatus.Success;
+        }
 
-		public async Task WakeUp() {
-			Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
-			s.EnableBroadcast = true;
-			IPAddress broadcast = IPAddress.Parse("255.255.255.255");
-			byte[] prefix = new byte[] { 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F };
-			var pureMac = MacToByteArrayToSend(MAC);
-			byte[] sendbuf = prefix;
-			for (int i = 1; i <= 16; i++) {
-				sendbuf = sendbuf.Concat(pureMac).ToArray();
-			}
-			IPEndPoint ep = new IPEndPoint(broadcast, 9);
-			await s.SendToAsync(sendbuf, SocketFlags.None, ep);
-			byte[] MacToByteArrayToSend(string mac) {
-				byte[] bytes = new byte[6];
-				var hexStrings = mac.Split(":");
-				for (int i = 0; i < 6; i++) {
-					bytes[i] = Convert.ToByte(hexStrings[i], 16);
-				}
-				return bytes;
-			}
+        public async Task WakeUp()
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            s.EnableBroadcast = true;
+            var lastDot = IP.LastIndexOf(".");
+            IPAddress broadcast = IPAddress.Parse(IP.Substring(0, lastDot + 1) + "255");
+            Console.WriteLine(broadcast);
+            byte[] prefix = new byte[] { 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F, 0xF_F };
+            var pureMac = MacToByteArrayToSend(MAC);
+            byte[] sendbuf = prefix;
+            for (int i = 1; i <= 16; i++)
+            {
+                sendbuf = sendbuf.Concat(pureMac).ToArray();
+            }
+            IPEndPoint ep = new IPEndPoint(broadcast, 9);
+            await s.SendToAsync(sendbuf, SocketFlags.None, ep);
+            byte[] MacToByteArrayToSend(string mac)
+            {
+                byte[] bytes = new byte[6];
+                var hexStrings = mac.Split(":");
+                for (int i = 0; i < 6; i++)
+                {
+                    bytes[i] = Convert.ToByte(hexStrings[i], 16);
+                }
+                return bytes;
+            }
 
-		}
-	}
+        }
+    }
 }
