1a9900dfd414fc85b1919791d7dad6a7144a6336
Ragnar Stølsmark <rstolsmark@live.no>
2024-06-02 08:40:24 +0200

    Added infrastructure for api

--- Configuration/AzureAdConfiguration.cs
+++ Configuration/AzureAdConfiguration.cs
@@ -1,28 +1,40 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
+using Microsoft.IdentityModel.Logging;
 
 namespace Rstolsmark.WakeOnLanServer.Configuration;
 
 public static class AzureAdConfiguration
 {
     public static void AddAzureAdAuthentication(this WebApplicationBuilder builder, IMvcBuilder mvcBuilder,
-        IConfigurationSection azureAdConfiguration, IEnumerable<PolicyRole> policyRoles)
+        IConfigurationSection azureAdConfiguration)
     {
         mvcBuilder.AddMicrosoftIdentityUI();
+
         builder.Services
             .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-            .AddMicrosoftIdentityWebApp(azureAdConfiguration);
-        builder.Services.AddAuthorization(options =>
-        {
-            foreach (var policyRole in policyRoles)
+                .AddMicrosoftIdentityWebApp(azureAdConfiguration);
+
+        builder.Services
+            .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApi(azureAdConfiguration);
+
+        builder.Services.AddAuthentication()
+            .AddPolicyScheme("OpenIdConnect_Or_JWT", "OpenIdConnect_Or_JWT", options =>
             {
-                options.AddPolicy(policyRole.Policy, policy => policy.RequireRole(policyRole.Role));
-            }
-            options.FallbackPolicy = new AuthorizationPolicyBuilder()
-                .RequireAuthenticatedUser()
-                .Build();
+                options.ForwardDefaultSelector = context =>
+                    context.Request.Path.StartsWithSegments("/api")
+                        ? JwtBearerDefaults.AuthenticationScheme
+                        : OpenIdConnectDefaults.AuthenticationScheme;
+            });
+        
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultScheme = "OpenIdConnect_Or_JWT";
+            options.DefaultChallengeScheme = "OpenIdConnect_Or_JWT";
         });
     }
     
