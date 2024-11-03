72789ba4f891fa1243ced440f015c2b2b52e2bcb
Ragnar Stølsmark <rstolsmark@live.no>
2024-11-03 08:04:58 +0100

    Specify csproj file for publish

--- Release.ps1
+++ Release.ps1
@@ -7,7 +7,7 @@ $releaseDate = Get-Date -Format "yyyy-MM-dd"
 git add changelog.md
 git commit -m "Updated changelog with release $version"
 git tag $version
-dotnet publish -c Release -p:Version=$version -o release
+dotnet publish Rstolsmark.WakeOnLanServer.csproj -c Release -p:Version=$version -o release
 Compress-Archive -Force -Path release/* -DestinationPath Rstolsmark.WakeOnLanServer.zip
 Remove-Item release -Recurse -Force
 docker login -u rstolsmark
