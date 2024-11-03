16f95761defab1ea2864143c17c7f753acafb7e0
Ragnar Stølsmark <rstolsmark@live.no>
2024-11-03 08:16:39 +0100

    Tag docker image with lates

--- Release.ps1
+++ Release.ps1
@@ -12,5 +12,6 @@ Compress-Archive -Force -Path release/* -DestinationPath Rstolsmark.WakeOnLanSer
 Remove-Item release -Recurse -Force
 docker login -u rstolsmark
 docker build --build-arg VERSION=$version -t rstolsmark/wakeonlanserver:$version .
-docker push rstolsmark/wakeonlanserver:$version
+docker tag rstolsmark/wakeonlanserver:$version rstolsmark/wakeonlanserver:latest
+docker push --all-tags rstolsmark/wakeonlanserver:$version
 docker logout
